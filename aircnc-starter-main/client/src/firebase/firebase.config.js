9c308133594ba52dd7a17387e3d8bf5398d2fdd6
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2023-11-10 23:52:05 +0600

    roomvue

---
+++ aircnc-starter-main/client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  // COnfig code goes here
+  
+    apiKey: "AIzaSyCQoQq4LtmYs-nMJu63232ENncx43w6_UA",
+    authDomain: "aircnc-phero.firebaseapp.com",
+    projectId: "aircnc-phero",
+    storageBucket: "aircnc-phero.appspot.com",
+    messagingSenderId: "80844565053",
+    appId: "1:80844565053:web:7b8104bdbc487b75d39854"
+  
+}
+
+const app = initializeApp(firebaseConfig)
+
+export default app
